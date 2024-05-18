4c32c53a35989a32fd372cf6ab9151f248a2b58e
Asai Toshiya <to.asai.60@gmail.com>
2024-05-18 21:33:45 +0900

    ワンライナーにリファクタリングする

--- FindFileByHashInFolder.ps1
+++ FindFileByHashInFolder.ps1
@@ -1,12 +1,3 @@
 # 使い方: .\FindFileByHashInFolder.ps1 <検索されるフォルダのパス> <検索するファイルのパス>
 
-param(
-    [string]$targetFolder,
-    [string]$targetFile
-)
-
-$targetHash = (Get-FileHash -Path $targetFile).Hash
-
-Get-ChildItem -Path $targetFolder -File |
-    ? { (Get-FileHash -Path $_.FullName).Hash -eq $targetHash } |
-    % { Write-Output "$($_.Name)" }
+Get-ChildItem -Path $Args[0] -File | ? { (Get-FileHash -Path $_.FullName).Hash -eq (Get-FileHash -Path $Args[1]).Hash } | % { Write-Output "$($_.Name)" }
