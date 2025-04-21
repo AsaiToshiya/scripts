0a4a7a64e11392bd35bad3a66501a21ae1efb5c0
AsaiToshiya <to.asai.60@gmail.com>
2025-04-22 00:16:40 +0900

    Add files via upload

---
+++ send_ctrl_r.ps1
@@ -0,0 +1,10 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+while ($true) {
+    # Ctrl+R を送信
+    [System.Windows.Forms.SendKeys]::SendWait("^{r}")
+    
+    # 3分待機
+    Start-Sleep -Seconds 180
+}
